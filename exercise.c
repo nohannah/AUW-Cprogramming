e0faea1968760e8a584780c3f35c3fb16e886423
nohannah <nohannah508@gmail.com>
2023-10-12 15:01:14 +0600

    practice

---
+++ exercise.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+int main()
+{
+    // double x,y,z ,result;
+    
+    // printf("x:");
+    // scanf("%lf",&x);
+    // printf("y:");
+    // scanf("%lf",&y);
+    // printf("z:");
+    // scanf("%lf",&z);
+    // result=x*y+y*z+z*x;
+    // printf("compute:. %.2lf",result);
+
+    int a,b,h ;
+    double area;
+    printf("enter num of a:");
+    scanf("%d",&a);
+    printf("enter num of b:");
+    scanf("%d",&b);
+    printf("enter num of h:");
+    scanf("%d",&h);
+    area=1.0/2.0*(a+b)*h;
+    printf("result of area: %.2lf",area);
+
+
+    return 0;
+    
+}
