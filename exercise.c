085317be1d87d843d39804498cb387a3f11ef100
nohannah <nohannah508@gmail.com>
2023-10-15 14:29:22 +0600

    pow function

--- exercise.c
+++ exercise.c
@@ -14,18 +14,26 @@ int main()
     // result=x*y+y*z+z*x;
     // printf("compute:. %.2lf",result);
 
-    int a,b,h ;
-    double area;
-    printf("enter num of a:");
-    scanf("%d",&a);
-    printf("enter num of b:");
-    scanf("%d",&b);
-    printf("enter num of h:");
-    scanf("%d",&h);
-    area=1.0/2.0*(a+b)*h;
-    printf("result of area: %.2lf",area);
+     //area of triangle 
+    // int a,b,h ;
+    // double area;
+    // printf("enter num of a:");
+    // scanf("%d",&a);
+    // printf("enter num of b:");
+    // scanf("%d",&b);
+    // printf("enter num of h:");
+    // scanf("%d",&h);
+    // area=1.0/2.0*(a+b)*h;
+    // printf("result of area: %.2lf",area);
 
-
+        // math function 
+        int x , y ,z;
+        printf("enter number of x:");
+        scanf("%d",&x);
+        printf("enter number of y:");
+        scanf("%d",&y);
+        z=pow(x,y);
+        printf("%d in the power of %d : is  %d ", x,y, z);
     return 0;
     
 }
