ae353f20c25506e2c051a686c91ea40f72d3e3de
nohannah <nohannah508@gmail.com>
2023-10-26 15:06:04 +0600

    using elseif

--- leap_year.c
+++ leap_year.c
@@ -12,6 +12,17 @@ int main (){
     else {
         printf("not leap year");
     }
+    // //using elseif 
+    // if (year%4==0 && year%100!=0)
+    // {
+    //     printf("Leap year");
+    // }
+    // else if( year%400==0){
+    //     prinft ("leap year");
+    // }
+    // else {
+    //     printf("not leap year");
+    // }
 
     return 0;
 }
