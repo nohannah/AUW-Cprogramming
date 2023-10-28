5b2634ae1422a2458202b5274229d76050fe9978
nohannah <nohannah508@gmail.com>
2023-10-28 15:13:48 +0600

    nested if else

--- Practice Program/practice3.c
+++ Practice Program/practice3.c
@@ -5,28 +5,32 @@ int main(){
 
     printf("enter the score :");
     scanf("%d",&score);
+    if (score>=0 && score<100)
+    {
+        if (score>=90 && score <=100)
+        {
+            printf("A");
+        }
+        else if (score>=80 && score <=89)
+        {
+            printf("B");
+        }
+        else if (score>=70 && score <=79)
+        {
 
-    if (score>=90 && score <=100)
-    {
-        printf("A");
-    }
-    else if (score>=80 && score <=89)
-    {
-        printf("B");
-    }
-    else if (score>=70 && score <=79)
-    {
-
-        printf("C");
-    }
-    else if (score>=60 && score <=69)
-    {
-        printf("D");
-    }
+            printf("C");
+        }
+        else if (score>=60 && score <=69)
+        {
+            printf("D");
+        }
+        else {
+            printf("F");
+        }
+    } 
     else {
-        printf("F");
+        printf("invalid value");
     }
-    
 
     return 0;
 }
